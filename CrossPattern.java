da0d324b8b864ba290ab6a281e6a5f26411c166b
sanchibansal <bansalsanchi77.sb@gmail.com>
2017-05-04 20:43:06 +0530

    code

--- CrossPattern.java
+++ CrossPattern.java
@@ -1,21 +1,22 @@
 import java.util.*;
 class CrossPattern {
-        public static void main(String[] args) {
-            Scanner s = new Scanner(System.in);
-            System.out.println("enter the number of rows/columns");
-            int n = s.nextInt();			//inputting number of rows/columns through scanner
-            int i, j;
-            for (i = 1; i <= n; i++) {
-                for (j = 1; j <= n; j++) {
-                    if (j == i) {			//prints * on the principle diagonal
-                        System.out.print("*");		
-                    } else if (j == n - (i - 1)) {
-                        System.out.print("*");		//prints * on the secondary diagonal
-                    } else {
-                        System.out.print(" ");
-                    }
-                }
-                System.out.println();
-            }
-        }
-    }
+    public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("enter the number of rows/columns");
+    int n = s.nextInt();			//inputting number of rows/columns through scanner
+    for (int i = 1; i <= n; i++) {		// first loop for row
+         for (int j = 1; j <= n; j++) {		// second loop for column
+             if (j == i) {			
+             System.out.print("*");		//prints * for principle diagonal
+             }
+             else if (j == n - (i - 1)) {
+             System.out.print("*");		//prints * for secondary diagonal
+             }
+             else {
+             System.out.print(" ");
+             }
+          }
+          System.out.println();			// printing new line after one iteration
+     }
+     }
+}
